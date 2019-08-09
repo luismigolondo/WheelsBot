47985a2b3b74bb64d091b1deedf626e86c703fb7
LUIS MIGUEL GOMEZ LONDOÐO <lm.gomezl@sis.virtual.uniandes.edu.co>
2019-08-09 11:37:41 -0500

    Driver chrome

--- wheelsbot.py
+++ wheelsbot.py
@@ -1,1 +1,5 @@
-print("hello world!")
+from selenium import webdriver
+
+driver = webdriver.Chrome("\\CODD.sis.virtual.uniandes.edu.co\Estudiantes\Profiles\lm.gomezl\Desktop\WheelsBot\lib\chromedriver.exe")
+driver.set_page_load_timeout("10")
+driver.get("http://google.com")
